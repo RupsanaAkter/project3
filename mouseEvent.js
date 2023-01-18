cb7e292c78cd7adb15d5b4d0d2ccbf534061ae3f
R7558 <rupsanaworld1004@gmail.co>
2023-01-18 23:18:24 +0600

    first commit

---
+++ mouseEvent.js
@@ -0,0 +1,61 @@
+
+
+
+const div=document.querySelector('div');
+div.addEventListener('click',function(e){
+    // console.log('click');
+// console.log(e.target);
+// console.log(e.target.id);
+// console.log(e.target.className);
+console.log(e.target.innerHTML);
+console.log(e.target.innerText);
+console.log(e.target.textContent);
+
+
+
+
+
+
+})
+
+
+// div.addEventListener('dblclick',function(){
+//     console.log('dblclick');
+// })
+
+// div.addEventListener('mousedown',function(){
+//     console.log(' mousedown');
+// })
+
+// div.addEventListener('mouseup',function(){
+//     console.log('mouseup');
+// })
+
+// div.addEventListener('mouseenter',function(){
+//     console.log(' mouseenter');
+// })
+
+// div.addEventListener('mouseleave',function(){
+//     console.log('mouseleave');
+// })
+
+// div.addEventListener('mousemove',function(e){
+//     // console.log(' mousemove');
+
+//     //windows compare X and Y access value----
+// /*---------------
+// console.log('clientX = '+e.clientX + ",clientY="+e.clientY);
+// -------------------*/
+
+
+// //div accesss X and Y value--------
+
+// console.log('offsetX = '+e.offsetX + ",offsetY="+e.offsetY);
+
+
+
+// })
+
+// div.addEventListener('mouseover',function(){
+//     console.log('mouseover');
+// })
